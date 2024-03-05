18860ddb4ea00eb5e97028bc5c20b160f969b864
Settter <egor-udon@mail.ru>
2024-03-05 22:56:06 +0300

    изменение перед отправкой клиенту

--- bot.py
+++ bot.py
@@ -8,29 +8,31 @@ bot = telebot.TeleBot('5266363672:AAEyg1z8QrEwfS31bJUhGybWZs7zpveD7wY')
 @bot.message_handler(commands=['start'])
 def start_handler(message):
     user_id = str(message.from_user.id)
-    if open('bot_save_id_YoN.txt', 'r').read().find('1') != -1:
-        ids = open('bot_user_id.txt', 'r').read()
+    if open('configuration_files/save_id_rule.txt', 'r').read().find('1') != -1:
+        ids = open('service_files/user_id_file.txt', 'r').read()
         if ids.find(user_id) == -1:
             bot.send_message(message.chat.id,'Добрый день ' + message.from_user.first_name + ", вы подписались на уведомления об обновлениях в группах.")
-            with open('bot_user_id.txt', 'w') as file:
+            with open('service_files/user_id_file.txt', 'w') as file:
                 file.write(ids + user_id + ' ')
     pass
 
 
 def mass_message(keyword, group_link, group_id, post_id):
-    ids = open('bot_user_id.txt', 'r').read().split(' ')
+    ids = open('service_files/user_id_file.txt', 'r').read().split(' ')
     for i in range(len(ids)):
         if ids[i] != '':
             bot.send_message(int(ids[i]),
-                             'В группе ' + group_link + ' вышел пост с ключевым словом ' + keyword + ' - ' + group_link + '?w=wall-' + str(
+                             'В группе ' + group_link + ' вышел подходящий пост - https://vk.com/' + group_link + '?w=wall-' + str(
                                  group_id) + '_' + str(post_id))
 
 
 def bot_main():
     try:
+        bot.delete_webhook()
         bot.polling(none_stop=True)
     except Exception as e:
         print(e)
         time.sleep(15)
+
 
 
--- main.py
+++ main.py
@@ -7,42 +7,52 @@ import requests
 from bot import mass_message, bot_main
 
 
-def get_post_link(_id, _domain, offset, count):
-    # без offset выведет закрем, если он есть, а с offset выведет пост исключая закреп (offset обозначает с какого
+def get_post_link(owner_id, domain, offset, count):
+    # без offset выведет закреп, если он есть, а с offset выведет пост исключая закреп (offset обозначает с какого
     # поста начать)
     immutable_link = 'https://api.vk.com/method/'
     method = 'wall.get'
     for_who = 'all'
     access_token = '50934c3550934c3550934c354550e8167b5509350934c353283b43e2d4451cd19e0fafb'
     version = '5.131'
-    return immutable_link + method + "?owner_id=" + _id + '&domain=' + _domain + '&offset=' + offset + '&count=' + count + '&filter=' + for_who + '&access_token=' + access_token + '&v=' + version
+    return immutable_link + method + "?owner_id=-" + owner_id + '&domain=' + domain + '&offset=' + offset + '&count=' + count + '&filter=' + for_who + '&access_token=' + access_token + '&v=' + version
 
 
-def get_first_post(owner_, domain_, offset='0'):
-    posts = requests.get(
-        get_post_link(_id=owner_, _domain=domain_, offset=offset,
-                      count='1')).json()
+def get_first_post(owner_id, domain):
     try:
-        if posts['response']['items'][0]['is_pinned'] == 1:
-            return requests.get(get_post_link(_id=owner_, _domain=domain_, offset='1', count='1')).json()
-    except KeyError:
-        return posts
-    except IndexError:
-        print("Group haven't posts yet")
+        posts = requests.get(get_post_link(owner_id, domain, '0', '1')).json()
+        if 'response' in posts and 'items' in posts['response'] and posts['response']['items']:
+            first_post = posts['response']['items'][0]
+            if 'is_pinned' in first_post and first_post['is_pinned'] == 1:
+                return requests.get(get_post_link(owner_id, domain, '1', '1')).json()
+            else:
+                return posts
+        else:
+            print("No items found in the response")
+            return None
+    except Exception as e:
+        print(f"Error getting the first post: {e}")
+        return None
 
 
-def get_post_text(owner_id_, domain_):
-    first_post_ = get_first_post(owner_=owner_id_, domain_=domain_)
-    attachments = first_post_['response']['items'][0]['attachments']
-    additional_text = first_post_['response']['items'][0]['text']
-    try:
-        type_post = attachments[0]['type']
-        post = attachments[0][type_post]['description']
-        return post + '\n' + additional_text
-    except KeyError:
-        return additional_text
-    except IndexError:
-        return additional_text
+def get_post_text(owner_id, domain):
+    first_post = get_first_post(owner_id, domain)
+    if first_post and 'response' in first_post and 'items' in first_post['response'] and first_post['response']['items']:
+        try:
+            attachments = first_post['response']['items'][0].get('attachments', [])
+            additional_text = first_post['response']['items'][0].get('text', '')
+            post = additional_text
+            if attachments:
+                type_post = attachments[0]['type']
+                post = attachments[0][type_post]['description']
+            return post + '\n' + additional_text
+        except KeyError:
+            print("Key error while parsing the response")
+            return None
+    else:
+        print("No items found in the response")
+        return None
+
 
 
 def get_owner_id(link):
@@ -52,9 +62,6 @@ def get_owner_id(link):
                          'response']['object_id'])
 
 
-# print(get_first_post(owner_id='-38029', domain='https://vk.com/synergyuniversity')['response']['items'][0]['id'])
-
-
 def write_ids_file(now_domain, orig_file, new_id):
     if orig_file.find(now_domain + ' = ') != -1:  # если есть домен и id, то вывести id
         post_ids = orig_file.split('  ')
@@ -62,29 +69,29 @@ def write_ids_file(now_domain, orig_file, new_id):
             if post_ids[z].find(now_domain + ' = ') != -1:
                 return post_ids[z].replace(now_domain + ' = ', '', 1)
     else:  # если нет домена, то записать домен
-        f1 = open('postIdis.txt', 'r', encoding="utf-8").read()
-        with open('postIdis.txt', 'w') as ff:
+        f1 = open('service_files/post_id_file.txt', 'r', encoding="utf-8").read()
+        with open('service_files/post_id_file.txt', 'w') as ff:
             ff.write(f1 + str(now_domain) + ' = ' + str(new_id) + '  ')
         return 1
 
 
 def write_new_id(link, num):
-    f1 = open('postIdis.txt', 'r', encoding="utf-8").read()
+    f1 = open('service_files/post_id_file.txt', 'r', encoding="utf-8").read()
     split = f1.split('  ')
     for j in range(len(split)):
         if split[j].find(link) != -1:
             f1 = f1.replace(split[j] + '  ', link + ' = ' + str(num) + '  ', 1)
-    with open('postIdis.txt', 'w') as ff:
+    with open('service_files/post_id_file.txt', 'w') as ff:
         ff.write(f1)
 
 
 def post_id_check():
-    ids_text = open('postIdis.txt', 'r', encoding="utf-8").read()
+    ids_text = open('service_files/post_id_file.txt', 'r', encoding="utf-8").read()
     ids = ids_text.split('  ')
-    groups = open('groups.txt', 'r', encoding="utf-8").read()
+    groups = open('configuration_files/groups.txt', 'r', encoding="utf-8").read()
     for i in range(len(ids)):
         if groups.find(re.sub(r' = \d+', '', ids[i])) == -1:
-            with open('postIdis.txt', 'w') as ff:
+            with open('service_files/post_id_file.txt', 'w') as ff:
                 ff.write(ids_text.replace(ids[i] + '  ', '', 1))
 
 
@@ -92,28 +99,28 @@ def main():
     while True:
         post_id_check()
         time.sleep(1)
-        f = open('postIdis.txt', 'r', encoding="utf-8").read()
-        domains = open('groups.txt', 'r', encoding="utf-8").read().split(' ')
+        f = open('service_files/post_id_file.txt', 'r', encoding="utf-8").read()
+        domains = open('configuration_files/groups.txt', 'r', encoding="utf-8").read().split(' ')
         for x in range(len(domains)):
-            domain = domains[x]
+            domain = domains[x].replace('https://vk.com/', '', 1)
             owner_id = get_owner_id(domain)
             time.sleep(1)
-            first_post = get_first_post(owner_=owner_id, domain_=domain)
-            post_text = get_post_text(owner_id_=owner_id, domain_=domain)
-            current_id = first_post['response']['items'][0]['id']
-            post_id = write_ids_file(domain, f, current_id)
-            if int(post_id) != int(current_id) or post_id == 1:
-                write_new_id(domain, current_id)
-                file = open('keywords.txt', 'r', encoding="utf-8").read().lower()
-                keywords = file.split(' ')
-                keywords_mention = 0
-                for i in range(len(keywords)):
-                    if post_text.lower().find(keywords[i]) != -1:
-                        keywords_mention += 1
-                if keywords_mention >= len(keywords):
-                    mass_message(keyword=keywords[0], group_link=domain, group_id=owner_id, post_id=current_id)
-            time.sleep(2)
-        # print(get_post_text(owner_id='-38029', domain='https://vk.com/synergyuniversity'))
+            first_post = get_first_post(owner_id, domain)
+            post_text = get_post_text(owner_id, domain)
+            if first_post and post_text:
+                current_id = first_post['response']['items'][0]['id']
+                post_id = write_ids_file(domain, f, current_id)
+                if int(post_id) != int(current_id) or post_id == 1:
+                    write_new_id(domain, current_id)
+                    file = open('configuration_files/keywords.txt', 'r', encoding="utf-8").read().lower()
+                    keywords = file.split(' ')
+                    keywords_mention = 0
+                    for i in range(len(keywords)):
+                        if post_text.lower().find(keywords[i]) != -1:
+                            keywords_mention += 1
+                    if keywords_mention >= len(keywords):
+                        mass_message(keyword=keywords[0], group_link=domain, group_id=owner_id, post_id=current_id)
+                time.sleep(2)
         time.sleep(2)
 
 
